24625ada7f3c42dce385b521c07c0df55843e1ce
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-23 13:03:34 +0200

    🎉 Add : main header component with styles
    Introduces the MainHeader React component and its associated CSS module. The header includes a logo, navigation links, and responsive styling.

---
+++ components/main-header/main-header.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavLink from "./nav-link";
+import logoImg from '../../assets/logo.png';
+import classes from './main-header.module.css';
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground/>
+
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImg} alt="Website log image" priority={true}/>
+                    Chaska Food Point
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">Foodies Community</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+
+    );
+}
